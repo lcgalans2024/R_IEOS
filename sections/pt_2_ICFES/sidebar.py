5f9215dae64cc4a4c665dbf54c04f1ec4378de00
lcgalans2024 <lcgalans2024@gmail.com>
2026-06-08 21:18:22 -0500

    Quitamos el filtro de año de los filtros generales

--- sections/pt_2_ICFES/sidebar.py
+++ sections/pt_2_ICFES/sidebar.py
@@ -5,12 +5,15 @@ def render_sidebar():
         #st.header("🧭 Sidebar — Pantalla 1")
         # Selector de grado
         grupos = ["Todos","1101", "1102", "1103", "1104"]
-        grupo = st.selectbox("Grado", grupos, key="p1_grado")
-        anio = st.selectbox("Año", ["2026","2025", "2024"], key="p1_anio")
+        grupo = st.selectbox("Grupo", grupos, key="p1_grado")
+        #anio = st.selectbox("Año", ["2026","2025", "2024"], key="p1_anio")
         
-        if anio == "2020":
+        #if anio == "2020":
             # -- Select for high sample rate data
-            high_fs = st.checkbox('Sin conectar')
+        #    high_fs = st.checkbox('Sin conectar')
 
         #piar = st.checkbox('Sin PIAR')
-    return {"anio": anio, "grupo": grupo, "high_fs": high_fs if anio == "2020" else False}#, "piar": piar}
+    return {#"anio": anio,
+            "grupo": grupo,
+            #"high_fs": high_fs if anio == "2020" else False
+            }#, "piar": piar}
